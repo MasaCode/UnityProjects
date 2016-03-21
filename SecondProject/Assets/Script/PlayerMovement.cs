bbf4d1d75a6d1d0edf18cf21b996c0bffe959578
Masa <masashi.morita@itdcanada.ca>
2016-03-20 21:40:08 -0700

    This is my first Projects.

---
+++ SecondProject/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float moveSpeed;
+	public float maxSpeed = 5;
+    private Vector3 location;
+	private float maxX = 250;
+
+    // Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		location = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
+		transform.Translate (location * moveSpeed);
+	}
+}
