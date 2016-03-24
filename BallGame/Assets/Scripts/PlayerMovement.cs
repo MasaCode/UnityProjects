bd15eebade0f1f277022f07480563de9fd407447
Masa <masashi.morita@itdcanada.ca>
2016-03-24 08:35:14 -0700

    I changed the player of game.

--- BallGame/Assets/Scripts/PlayerMovement.cs
+++ BallGame/Assets/Scripts/PlayerMovement.cs
@@ -11,7 +11,31 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 place = new Vector3 (Input.GetAxis ("Horizontal"), 0f, Input.GetAxis ("Vertical"));
+		/*
+        Vector3 place = new Vector3 (Input.GetAxis ("Horizontal"), 0f, Input.GetAxis ("Vertical"));
 		transform.Translate(place*speed);
+         */
+        float x = transform.position.x, y = transform.position.y, z = transform.position.z;
+      
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            x += speed;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            x -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            z += speed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            z -= speed;
+        }
+
+        transform.position = new Vector3(x, y, z);
 	}
 }
