a99a3eb69e9229d46111ff1895372ea4a94e557b
MoritaMasashi <moritamasashi@MoritaMasashi-no-MacBook-Air.local>
2016-03-24 15:50:47 -0700

    I change the enemy's movement.

---
+++ BallGame/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+	[SerializeField]
+	private float speedX;
+	[SerializeField]
+	private float speedZ;
+
+	private Vector3 placement;
+
+
+	// Use this for initialization
+	void Start () {
+		RandomSpeed ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 placement = transform.position;
+		placement.x += speedX;
+		placement.z += speedZ;
+
+		if (placement.x >= 50f) {
+			placement.x = 49f;
+			RandomSpeed ();
+			speedX = -speedX;
+		} else if (placement.x <= -50f) {
+			placement.x = -49f;
+			RandomSpeed ();
+		}
+
+		if (placement.z >= 50f) {
+			placement.z = 49f;
+			RandomSpeed ();
+			speedZ = -speedZ;
+		} else if (placement.z <= -50f) {
+			placement.z = -49f;
+			RandomSpeed ();
+		}
+
+		transform.position = placement;
+	}
+
+	private void RandomSpeed(){
+		speedX = Random.Range (0.1f, 0.5f);
+		speedZ = Random.Range (0.1f, 0.5f);
+	}
+}
